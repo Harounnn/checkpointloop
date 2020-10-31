d4f9558c27224ed17416651c03ab52776cc1cc1f
Harounnn <67702192+Harounnn@users.noreply.github.com>
2020-10-31 15:28:55 +0100

    Add files via upload

---
+++ chkptscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chkptscript : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(DestroyZombies());
+    }
+    IEnumerator DestroyZombies()
+    {
+        yield return new WaitForSeconds(5f);
+        GameObject[] zombies = GameObject.FindGameObjectsWithTag("Zombie");
+        foreach(GameObject g in zombies)
+        {
+            Destroy(g);
+        }
+    }
+}
